6480c70e6a6e03105ce82af7e74fcae0f85a9849
AdrikLarreategui <adrian15186@hotmail.com>
2023-11-06 19:32:39 +0100

    commit

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const APIKey = "9e9cd7ce5dfc1c9de8d770d482d83132";
 const TokenAPI = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTljZDdjZTVkZmMxYzlkZThkNzcwZDQ4MmQ4MzEzMiIsInN1YiI6IjY1NDYxMjYyNDFhNTYxMzM2OTNhMmEwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kFUENuVrI79BXjHDt6wbsL3mLbJhGvNnmZI6rrq6xMg"
 const URL = "https://api.themoviedb.org/3/movie/550?api_key=9e9cd7ce5dfc1c9de8d770d482d83132";
 
+const movieContainer = document.querySelector('#title')
 const formSearch = document.getElementById('form')
 const searchInput = document.getElementById('searchId')
 
+cont options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authoritation: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTljZDdjZTVkZmMxYzlkZThkNzcwZDQ4MmQ4MzEzMiIsInN1YiI6IjY1NDYxMjYyNDFhNTYxMzM2OTNhMmEwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kFUENuVrI79BXjHDt6wbsL3mLbJhGvNnmZI6rrq6xMg'
+    }
+};
