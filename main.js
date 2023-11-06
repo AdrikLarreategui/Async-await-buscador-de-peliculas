b3d24027fd3ef371dd79ff7553ac9551e43277eb
AdrikLarreategui <adrian15186@hotmail.com>
2023-11-06 14:08:20 +0100

    commit

---
+++ main.js
@@ -0,0 +1,3 @@
+const APIKey = "9e9cd7ce5dfc1c9de8d770d482d83132";
+const TokenAPI = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTljZDdjZTVkZmMxYzlkZThkNzcwZDQ4MmQ4MzEzMiIsInN1YiI6IjY1NDYxMjYyNDFhNTYxMzM2OTNhMmEwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kFUENuVrI79BXjHDt6wbsL3mLbJhGvNnmZI6rrq6xMg"
+const URL = "https://api.themoviedb.org/3/search/movie";
