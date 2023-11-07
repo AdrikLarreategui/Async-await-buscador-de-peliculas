7e090c894223bfafcfd21c764d5d1876448678cd
AdrikLarreategui <adrian15186@hotmail.com>
2023-11-07 12:34:44 +0100

    commit

--- main.js
+++ main.js
@@ -1,29 +1,23 @@
-const APIKey = "9e9cd7ce5dfc1c9de8d770d482d83132";
+const APIKey = "a25d776e9ca63550cc426057cb76929d";
 const TokenAPI = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTljZDdjZTVkZmMxYzlkZThkNzcwZDQ4MmQ4MzEzMiIsInN1YiI6IjY1NDYxMjYyNDFhNTYxMzM2OTNhMmEwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kFUENuVrI79BXjHDt6wbsL3mLbJhGvNnmZI6rrq6xMg"
-const URL = "https://api.themoviedb.org/3/movie/550?api_key=9e9cd7ce5dfc1c9de8d770d482d83132";
+//const URL = "https://api.themoviedb.org/3/movie/550?api_key=9e9cd7ce5dfc1c9de8d770d482d83132";
 const APIURL = "https://api.themoviedb.org/3/search/movie"
 
 const moviesContainer = document.querySelector('#title')
 const formSearch = document.getElementById('form')
 const searchInput = document.getElementById('searchId')
 
-const options = {
-    method: 'GET',
-    headers: {
-        accept: 'application/json',
-        Authoritation: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTljZDdjZTVkZmMxYzlkZThkNzcwZDQ4MmQ4MzEzMiIsInN1YiI6IjY1NDYxMjYyNDFhNTYxMzM2OTNhMmEwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kFUENuVrI79BXjHDt6wbsL3mLbJhGvNnmZI6rrq6xMg'
-    }
-};
+
 
 const showMovies = (movies) => {
     moviesContainer.innerHTML = ''
     movies.forEach ((movie) => {
         moviesContainer.innerHTML += `
         <div class="card col-lg-3 col-xs-12 col-md-6 m-2">
-				<img src="${movie.image}" alt="Card image"/>
+				<img src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="image">
 				<div class="card-body">
-						<h3 class="card-header">${movie.name}</h3>
-						<h5 class="card-title">${movie.status}</h5>
+						<h3 class="card-header">${movie.original_title}</h3>
+						<h5 class="card-title">${movie.overview}</h5>
 					</div>
      	</div>
         `
@@ -35,7 +29,7 @@ const searchMovie = async (e) => {
     e.preventDefault()
     try {
         const search = searchInput.value
-        const res = await axios.get(`${URL}?query=${search}&include_adult=false&page=1&api_key=${APIKey}`)
+        const res = await axios.get(`${APIURL}?query=${search}&include_adult=false&page=1&api_key=${APIKey}`)
         const movies = res.data.results
         showMovies(movies)
     } catch (error) {
@@ -44,17 +38,4 @@ const searchMovie = async (e) => {
 }
 
 formSearch.addEventListener('submit', searchMovie)
-
-// const options = {
-//     method: 'GET',
-//     headers: {
-//       accept: 'application/json',
-//       Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5ZTljZDdjZTVkZmMxYzlkZThkNzcwZDQ4MmQ4MzEzMiIsInN1YiI6IjY1NDYxMjYyNDFhNTYxMzM2OTNhMmEwYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kFUENuVrI79BXjHDt6wbsL3mLbJhGvNnmZI6rrq6xMg'
-//     }
-//   };
-  
-//   fetch('https://api.themoviedb.org/3/search/movie?query=ghost&include_adult=false&language=en-US&page=1', options)
-//     .then(response => response.json())
-//     .then(response => console.log(response))
-//     .catch(err => console.error(err));
 
